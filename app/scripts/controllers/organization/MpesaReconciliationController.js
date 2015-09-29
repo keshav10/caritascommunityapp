01bb6039bf3bcfe079b36a00132e67b1711d7454
keshav10 <keshav.mishra@confluxtechnologies.com>
2015-09-29 09:29:34 +0530

    mpesascreen

--- app/scripts/controllers/organization/MpesaReconciliationController.js
+++ app/scripts/controllers/organization/MpesaReconciliationController.js
@@ -4,7 +4,10 @@
             scope.routeTo = function (id,mpesaamount,mpetxnsacode,txnDate,txnId) {
                 location.path('/clientpayments/' + id+'/'+mpesaamount+'/'+mpetxnsacode+'/'+txnDate+'/'+txnId);
             };
-
+            scope.status=false;
+            scope.mobileNo=false;
+            scope.TxnDate=false;
+            scope.find=true;
             scope.completetransaction=[];
             var deferred = $q.defer();
             $http.get("http://localhost:9292/mpesa/getunmappedtransactions").success(function(data) {
@@ -12,8 +15,25 @@
                 scope.completetransaction=data;
             });
 
-            scope.search=function(data){
-                alert(data);
+            scope.search=function(query){
+               if(query=='status'){
+                   scope.status=true;
+                   scope.mobileNo=false;
+                   scope.TxnDate=false;
+                   scope.find=false;
+               }
+                if(query=='TxnDate'){
+                    scope.TxnDate=true;
+                    scope.status=false;
+                    scope.mobileNo=false;
+                    scope.find=false;
+                }
+                if(query=='mobileNo'){
+                    scope.status=false;
+                    scope.mobileNo=true;
+                    scope.TxnDate=false;
+                    scope.find=false;
+                }
             }
 
             //scope.transactions={"id":5,"ipnId":2972,"origin":"MPESA","destination":"254700733153","timeStamp":null,"testMessage":"BM46ST941 Confirmed.on 6/7/11 at 10:49 PM Ksh8,723.00 received from RONALD NDALO 254722291067.Account Number 5FML59-01 New Utility balance is Ksh6,375,223.00","user":"123","password":"123","transactionCode":"BM46ST941","mobileNo":"9632587410","accountName":"5FML5901","transactionDate":"1307385000000","transactionTime":"10:49 PM","transactionAmount":"8723.000000","sender":"RONALD NDALO","status":"UT","clientId":13,"officeId":4};
